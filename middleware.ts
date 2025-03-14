7ac4cec369370a3cdfdbbc8d8a0c3d2f05937bd6
Bintang Fadhli <48087452+bintangf@users.noreply.github.com>
2025-03-14 16:25:57 +0000

    Add Telegram notification for shortlink access and update package version to 5.0.0

--- middleware.ts
+++ middleware.ts
@@ -5,40 +5,64 @@ export const config = {
   matcher: '/:path*',
 }
 
+async function sendTelegramMessage(message: string) {
+  const botToken = process.env.TELEGRAM_BOT_TOKEN;
+  const chatId = process.env.TELEGRAM_CHAT_ID;
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message,
+    }),
+  });
+}
+
 export async function middleware(req: NextRequest) {
+  try {
+    const path = req.nextUrl.pathname.replace('/', '')
 
-  const path = req.nextUrl.pathname.replace('/', '')
+    let endURL = await kv.get(path)
 
-  let endURL = await kv.get(path)
+    if (path) {
+      if (endURL) {
+        // Send notification to Telegram
+        const geo = req.geo || {};
+        const locationMessage = `Shortlink accessed: ${req.nextUrl.origin}/${path}\nLocation: ${geo.city}, ${geo.region}, ${geo.country}`;
+        await sendTelegramMessage(locationMessage);
 
-  if (path) {
-    if (endURL) {
+        // if endURL missing http/https, add it
+        if (!endURL.match(/^[a-zA-Z]+:\/\//)) {
+          endURL = 'http://' + endURL
+        }
+        return NextResponse.redirect(new URL(endURL))
 
-      // if endURL missing http/https, add it
-      if (!endURL.match(/^[a-zA-Z]+:\/\//)) {
-        endURL = 'http://' + endURL
+      } else {
+
+        // Search for a secure key
+        let allKeys = []
+        for await (const key of kv.scanIterator()) {
+          allKeys.push(key)
+        }
+        let secureKey = allKeys.find((key) => key.startsWith(path))
+        if (secureKey) {
+          return NextResponse.redirect(new URL(req.nextUrl.toString().replace('/' + path, '') + '/unlock?key=' + path.split('$')[0]))
+        }
+        else if (path.includes('$')) {
+          return NextResponse.redirect(new URL(req.nextUrl.toString().replace('/' + path, '') + '/unlock?key=' + path.split('$')[0] ))
+        }
+
+        return NextResponse.next()
       }
-      return NextResponse.redirect(new URL(endURL))
-
     } else {
-
-      // Search for a secure key
-      let allKeys = []
-      for await (const key of kv.scanIterator()) {
-        allKeys.push(key)
-      }
-      let secureKey = allKeys.find((key) => key.startsWith(path))
-      if (secureKey) {
-        return NextResponse.redirect(new URL(req.nextUrl.toString().replace('/' + path, '') + '/unlock?key=' + path.split('$')[0]))
-      }
-      else if (path.includes('$')) {
-        return NextResponse.redirect(new URL(req.nextUrl.toString().replace('/' + path, '') + '/unlock?key=' + path.split('$')[0] ))
-      }
-
       return NextResponse.next()
     }
-  } else {
+  } catch (error) {
+    console.error('Middleware error:', error)
     return NextResponse.next()
   }
-
 }
